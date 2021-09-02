989df9ee246f0bf2c7411f731047ce2a509e49d1
Roy Park <roypark2638@gmail.com>
2021-09-02 15:26:51 -0700

    environment setup

---
+++ EmojiArtist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArtist
+//
+//  Created by Roy Park on 9/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiArtist/EmojiArtistApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtistApp.swift
+//  EmojiArtist
+//
+//  Created by Roy Park on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
